3fe14d554f4f2fad50e8d0ce81453ff027918c05
Ahsan Ishaq <ahsan.ishaq@arabsea.com>
2024-07-11 13:17:29 +0500

    testing

---
+++ src/function_interface/Main.kt
@@ -0,0 +1,11 @@
+package function_interface
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("just testing")
+
+        }
+    }
+}
